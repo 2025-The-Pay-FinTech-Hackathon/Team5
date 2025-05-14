f9e981f724e0bfb00e2e8721d36c1aa9884f9a30
Yooaknow <dbdkfma1228@naver.com>
2025-05-15 00:41:30 +0900

    feat: 기록왕 뱃지 로직 추가

--- src/pages/ChildDashboard.jsx
+++ src/pages/ChildDashboard.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { checkAndUpdateBadges } from '../utils/badgeUtils';
+
 import {
   Container,
   Grid,
@@ -51,6 +53,12 @@ function ChildDashboard() {
       setChild(findChildById(user.id));
     }
   }, [user, location.pathname]);
+
+  useEffect(() => {
+    if (child) {
+      checkAndUpdateBadges(child);
+    }
+  }, [child]);
 
   // 최근 활동(미션, 퀴즈 등)
   const recentActivities = [
