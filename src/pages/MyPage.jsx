cca34c32d80353564306926fef4d11f99919adb2
Yooaknow <dbdkfma1228@naver.com>
2025-05-17 06:36:50 +0900

    라이언 크기 수정

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -242,8 +242,8 @@ const MyPage = () => {
           sx={{
             position: 'absolute',
             right: { xs: 0, md: 40 },
-            bottom: -30,
-            width: { xs: 120, md: 180, lg: 220 },
+            bottom: -142,
+            width: { xs: 160, md: 220, lg: 280 },
             height: 'auto',
             zIndex: 2,
             userSelect: 'none',
